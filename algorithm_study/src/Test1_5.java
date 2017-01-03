bd4203ed6bd453ee84957c5a2643a460c91aed8b
Taeim Kwon <k.taemee@gmail.com>
2017-01-03 19:50:56 +0900

    .

---
+++ algorithm_study/src/Test1_5.java
@@ -0,0 +1,20 @@
+/*
+문제 10172번
+아래 예제와 같이 개를 출력하시오.
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+
+
+public class Test1_5 {
+  
+ public static void main (String[] args) {
+   
+ System.out.printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+   
+ }
+
+}
