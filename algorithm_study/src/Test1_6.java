9fe5962a2fce17647a11d178386660b590cf1721
Taeim Kwon <k.taemee@gmail.com>
2017-01-05 22:48:22 +0900

    .

---
+++ algorithm_study/src/Test1_6.java
@@ -0,0 +1,11 @@
+/*문제 10718번
+두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.*/
+
+public class Test1_6 {
+  
+  public static void main (String[] args) {
+    
+    System.out.printf("강한친구 대한육군\n강한친구 대한육군");
+  }
+
+}
