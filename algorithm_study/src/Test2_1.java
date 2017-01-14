bef56e5771cebde997d457a81531c4c3f3c67c0f
Taeim Kwon <k.taemee@gmail.com>
2017-01-15 00:03:53 +0900

    .

---
+++ algorithm_study/src/Test2_1.java
@@ -0,0 +1,26 @@
+/*문제 10998번 
+두 수 A와 B를 입력받은 다음, A*B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A,B < 10)
+
+출력
+첫째 줄에 A*B를 출력한다.*/
+
+
+public class Test2_1 {
+  
+  public static void main (String[] args) throws Exception {
+ 
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        
+        String[] num = sc.nextLine().split(" ");
+
+        System.out.println(Integer.parseInt(num[0]) * Integer.parseInt(num[1]));
+            
+            
+                
+        
+  }
+
+}
